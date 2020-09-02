72a5d389f4bc76689f1fb18af178aae37ef6a2b9
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-09-02 18:34:54 +0530

    bmi completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 let x = 0;
+
+document.getElementById('bmi-form').addEventListener('submit', e => {
+	e.preventDefault();
+
+	let height =
+		parseInt(document.getElementById('bmi-height-feet').value) * 0.3048 + parseInt(document.getElementById('bmi-height-inches').value) * 0.0254;
+	height *= height;
+
+	let bmi = document.getElementById('bmi-weight').value / height;
+	let final = bmi.toFixed(2);
+
+	document.getElementById('bmi-output').value = final;
+});
